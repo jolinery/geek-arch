93b787b3f69174f02d47441ca1fdc890e26a634f
zhanglin16 <zhanglin16@xiaomi.com>
2020-07-13 21:29:50 +0800

    add:pressure test

---
+++ chapter-7/pressure/src/main/java/com/Main.java
@@ -0,0 +1,14 @@
+package com;
+
+/**
+ * @program: pressure
+ * @description:
+ * @author: zhanglin16
+ * @create: 2020-07-13 19:00
+ **/
+public class Main {
+    public static void main(String[] args){
+        PressureTest pressureTest = new PressureTest("https://www.baidu.com",200,10);
+        pressureTest.testing(95);
+    }
+}
